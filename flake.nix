968ee535727c4a591c6c2b8d6ba10705659b5752
j-brn <me@bricker.io>
2022-10-17 23:04:26 +0200

    Add libvirtdHooks module

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, utils, ... } @ inputs:
+    {
+      nixosModules.libvirtdHooks = import ./modules/libvirtdHooks.nix;
+    }
+    // utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nixpkgs-fmt
+          ];
+        };
+      }
+    );
+}
