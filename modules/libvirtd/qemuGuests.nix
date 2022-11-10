98d13c9e1b59eaa34ea8a29d65612a72f56e43b2
j-brn <me@bricker.io>
2022-11-10 18:09:54 +0100

    libvirtd: fix tmpfiles package

--- modules/libvirtd/qemuGuests.nix
+++ modules/libvirtd/qemuGuests.nix
@@ -28,10 +28,8 @@ with lib; let
   writeValidatedXml = name: document:
     (pkgs.runCommand "${name}.xml" { }
       ''
-        set -e
-        echo ${document} > domain.xml
-        ${pkgs.libvirt}/bin/virt-xml-validate domain.xml domain
-        echo ${document} > $out
+        echo ${document} > $out && \
+        ${pkgs.libvirt}/bin/virt-xml-validate ${name}.xml domain
       '');
 
   tmpfilesPackage =
