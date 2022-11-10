92b0707310201ebc37b337b6bcbf0300ecabc615
j-brn <me@bricker.io>
2022-11-10 17:10:04 +0100

    libvirt: add module to create qemu guests

---
+++ modules/libvirtd/qemuGuests.nix
@@ -0,0 +1,62 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+with lib; let
+  cfg = config.virtualisation.libvirt.qemuGuests;
+
+  questType = types.submodule {
+    options = {
+      config = mkOption {
+        type = types.str;
+        descripton = mdDoc ''
+          libvirtd domain xml
+        '';
+      };
+
+      autostart = mkOption {
+        type = types.bool;
+        default = false;
+        description = mdDoc ''
+          whether to start the guest on boot
+        '';
+      };
+    };
+  };
+
+  writeValidatedXml = name: document:
+    pkgs.runCommand "${name}.xml"
+      ''
+        echo ${document} > document.xml
+        virt-xml-validate document.xml domain
+        echo ${document} > $out
+      '';
+
+  tmpfilesPackage =
+    let
+      rules = mapAttrsToList
+        (name: guest:
+          let
+            document = writeValidatedXml name guest.config;
+            target = "/var/libvirt/qemu/" + (optional (guest.autostart) "autostart/") + "${name.xml}";
+          in
+          "L ${target} - - - - ${document}");
+    in
+    pkgs.writeTextFile "libvirtd-guests" concatStringsSep "\n" rules;
+
+in
+{
+  ### Interface ###
+
+  options.virtualisation.libvirt.guests = mkOption {
+    type = attrsOf (guestType);
+    default = { };
+    description = mdDoc ''
+      libvirtd guests
+    '';
+  };
+
+  ### Implementation ###
+  systemd.tmpfiles.packages = [ tmpfilesPackage ];
+}
