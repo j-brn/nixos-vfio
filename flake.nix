cb7ac701ace35f9fc997e32b78a0598b75e78efc
j-brn <me@bricker.io>
2023-04-12 22:16:24 +0200

    switch to flake.parts

--- flake.nix
+++ flake.nix
@@ -1,30 +1,32 @@
 {
   inputs = {
-    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
-    utils = { url = "github:numtide/flake-utils"; };
-    nix-std = { url = "github:chessai/nix-std"; };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nix-std.url = "github:chessai/nix-std";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    devshell.url = "github:numtide/devshell";
+
+    nixfmt = {
+      url = "github:serokell/nixfmt";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, utils, nix-std, ... } @ inputs:
-    let
-      std = nix-std.lib;
-    in
-    {
-      nixosModules = {
-        kvmfr = import ./modules/kvmfr.nix { inherit std; };
-      };
-    }
-    // utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nixpkgs-fmt
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devshell.flakeModule
+        inputs.pre-commit-hooks.flakeModule
+      ];
+      systems = [ "x86_64-linux" ];
+      perSystem = { system, ... }: {
+        formatter = inputs.nixfmt.packages.${system}.default;
+
+        devshells.default = {
+          packages = [
+
           ];
         };
-      }
-    );
+      };
+    };
 }
